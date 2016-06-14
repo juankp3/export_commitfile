c0d038c43711aba9395b6b7203b40fc7293e20b4
juan harold kuga palomino <kuga@MacBook-Pro-de-juan.local>
2016-06-14 17:24:25 -0500

    modificando index

--- index.php
+++ index.php
@@ -13,11 +13,58 @@ require_once dirname(__FILE__) . '/test/PHPGit_RepoTestHelper.php';
 <?php 
 	$repo = new PHPGit_Repository('/Users/kuga/Projects/kuga/export_commitfile');
 	
+
+
+	exec("git log",$output);
+
+	echo "<pre>";
+	var_dump($output);
+	echo "</pre>";
+
+
+	$history = array();
+	foreach($output as $line){
+    if(strpos($line, 'commit')===0){
+        if(!empty($commit)){
+            array_push($history, $commit);  
+            unset($commit);
+        }
+        $commit['hash']   = substr($line, strlen('commit'));
+    }
+    else if(strpos($line, 'Author')===0){
+    $commit['author'] = substr($line, strlen('Author:'));
+    }
+    else if(strpos($line, 'Date')===0){
+    $commit['date']   = substr($line, strlen('Date:'));
+    }
+    else{       
+    $commit['message']  .= $line;
+    }
+}
+
+echo "<pre>";
+var_dump($history);
+echo "</pre>";
+
+
+exit;
+
 	echo "<pre>";
 	var_dump($repo);
 	echo "</pre>";
 	//echo $repo->git('log --oneline');
-	echo $repo->git('log --name-status');
+	$res = $repo->git('log --name-status');
+	echo "<pre>";
+	var_dump($res);
+	echo "</pre>";
+
+	echo "<h1>---------- A ----------</h1>";
+
+	$r1 = explode('', $res);
+
+	echo "<pre>";
+	var_dump($r1);
+	echo "</pre>";
 
 
 ?>
